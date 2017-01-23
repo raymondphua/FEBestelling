3817cdfaa078bc4c452c0939550f43757cd2837c
paisan <paisan.rietbroek@gmail.com>
2017-01-23 14:56:54 +0100

    fronted login created.

---
+++ app/src/test/java/com/infosupport/team2/febestelling/LoginActivityTest.java
@@ -0,0 +1,13 @@
+package com.infosupport.team2.febestelling;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by paisanrietbroek on 23/01/2017.
+ */
+
+public class LoginActivityTest {
+    
+}
