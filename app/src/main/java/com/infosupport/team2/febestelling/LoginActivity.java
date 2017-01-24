ddfed9b8a798c9bc47bfdf950152f1c628ea990c
paisan <paisan.rietbroek@gmail.com>
2017-01-24 11:45:37 +0100

    authentication implemented

--- app/src/main/java/com/infosupport/team2/febestelling/LoginActivity.java
+++ app/src/main/java/com/infosupport/team2/febestelling/LoginActivity.java
@@ -2,13 +2,36 @@ package com.infosupport.team2.febestelling;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.infosupport.team2.febestelling.util.AppSingleton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.R.attr.breadCrumbShortTitle;
+import static android.R.attr.password;
 
 
 /**
@@ -17,16 +40,22 @@ import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
+    private ProgressDialog progressDialog;
     private EditText emailField, passwordField;
-    private String serverEmail, serverPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.login_layout);
+
+        progressDialog = new ProgressDialog(this);
 
         emailField = (EditText) findViewById(R.id.login_email);
         passwordField = (EditText) findViewById(R.id.login_password);
+
+        resetFields();
+
         Button loginBtn = (Button) findViewById(R.id.login_btn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -36,50 +65,109 @@ public class LoginActivity extends Activity {
                 String email = String.valueOf(emailField.getText());
                 String password = String.valueOf(passwordField.getText());
 
-                boolean isCorrect = validateIsCorrect(email, password);
-
-                if (isCorrect) {
-                    Toast.makeText(v.getContext(), "Inloggen is gelukt.", Toast.LENGTH_SHORT);
-                    goToNextPage(email, password, v);
-                } else {
-                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(v.getContext());
-                    alertDialogBuilder.setTitle("Inloggen mislukt");
-                    alertDialogBuilder.setPositiveButton("Opnieuw", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    alertDialogBuilder.show();
+                if (isValidInput(email, password)) {
+                    authenticateApp(createAuthUrl(email, password));
                 }
             }
         });
     }
 
-    public Boolean validateIsCorrect(String inputEmail, String inputPassword) {
-
-        // TODO: get data from server
-        setServerData("test@test.nl", "password");
-
-        if (!inputEmail.matches("") && !inputPassword.matches("")) {
-            return (serverEmail.equals(inputEmail)) &&
-                    (serverPassword.equals(inputPassword));
-        } else {
-            System.out.println("empty fields!");
-            return false;
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        resetFields();
     }
 
-    public void goToNextPage(String email, String password, View view) {
-        Intent intent = new Intent(view.getContext(), MainActivity.class);
-        intent.putExtra("email", email);
-        intent.putExtra("password", password);
+    public void goToNextPage() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
 
-    public void setServerData(String serverEmail, String serverPassword) {
-        this.serverEmail = serverEmail;
-        this.serverPassword = serverPassword;
+    public Boolean isValidInput(String email, String password) {
+        if (email.matches("") || password.matches("")) {
+            Toast.makeText(getApplicationContext(),
+                    "Vul alle velden in.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
     }
 
+    public String createAuthUrl(String email, String password) {
+        String AUTHENTICATION_URL = "http://10.0.3.2:11150/oauth/token?grant_type=password&" +
+                "username=" + email + "&" +
+                "password=" + password + "" +
+                "&client_id=kantilever&client_secret=kantiSecret";
+        return AUTHENTICATION_URL;
+    }
+
+    public void authenticateApp(String url) {
+        String REQUEST_TAG = "com.infosupport.team2.authenticationReq";
+        progressDialog.setMessage("Laden...");
+        progressDialog.show();
+
+        JSONObject jsonObject = new JSONObject();
+        final String requestBody = jsonObject.toString();
+
+        final StringRequest putRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject responseObject = new JSONObject(response);
+                            AppSingleton.getInstance(getApplicationContext()).token = responseObject.getString("access_token");
+                            toastMessage("Inloggen gelukt!");
+                            goToNextPage();
+                            progressDialog.hide();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.d("Response", response);
+                            progressDialog.hide();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        try {
+                            if (error.networkResponse == null || error.networkResponse.statusCode == 500) {
+                                toastMessage("Het is momenteel niet mogelijk om in te loggen.");
+                                progressDialog.hide();
+                            } else if (error.networkResponse.statusCode == 401){
+                                toastMessage("Login gegevens kloppen niet.");
+                                progressDialog.hide();
+                            }
+                            Log.d("Error.response", error.getMessage());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            progressDialog.hide();
+                        }
+                    }
+
+                })
+        {
+
+            @Override
+            public byte[] getBody() {
+                return requestBody.getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(putRequest, REQUEST_TAG);
+    }
+
+    public void resetFields() {
+        this.emailField.setText("");
+        this.passwordField.setText("");
+    }
+
+    public void toastMessage(String txt) {
+        Toast.makeText(getApplicationContext(), txt, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/infosupport/team2/febestelling/MainActivity.java
+++ app/src/main/java/com/infosupport/team2/febestelling/MainActivity.java
@@ -16,8 +16,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -49,7 +51,6 @@ public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
     ListView listView;
     private String ORDER_URL = "http://10.0.3.2:11130/orderservice/statuses";
-    private String AUTHENTICATION_URL = "http://10.0.3.2:11150/oauth/token?grant_type=password&username=pieter@hotmail.com&password=henkie&client_id=kantilever&client_secret=kantiSecret";
     private LinearLayout linearLayout;
 
     @Override
@@ -57,59 +58,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         progressDialog = new ProgressDialog(this);
-
-        authenticateApp(AUTHENTICATION_URL);
+        statusRequest(ORDER_URL);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         progressDialog.dismiss();
-    }
-
-    public void authenticateApp(String url){
-        String REQUEST_TAG = "com.infosupport.team2.authenticationReq";
-        progressDialog.setMessage("Laden...");
-        progressDialog.show();
-
-        JSONObject jsonObject = new JSONObject();
-        final String requestBody = jsonObject.toString();
-        String result = "";
-
-        StringRequest putRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("Response", response);
-                        try {
-                            JSONObject responseObject = new JSONObject(response);
-                            AppSingleton.getInstance(getApplicationContext()).token = responseObject.getString("access_token");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                        statusRequest(ORDER_URL);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Error.response", error.getMessage());
-                        progressDialog.hide();
-                    }
-                })
-        {
-            @Override
-            public byte[] getBody() {
-                return requestBody.getBytes();
-            }
-
-            @Override
-            public String getBodyContentType() {
-                return "application/json";
-            }
-        };
-        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(putRequest, REQUEST_TAG);
     }
 
     public void statusRequest(String url) {
@@ -135,8 +90,20 @@ public class MainActivity extends Activity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
-                progressDialog.hide();
+                try {
+                    if (error.networkResponse == null || error.networkResponse.statusCode == 500) {
+                        toastMessage("Het systeem is momenteel onbereikbaar.");
+                        progressDialog.hide();
+                    } else if (error.networkResponse.statusCode == 401){
+                        toastMessage("U dient opnieuw in te loggen.");
+                        progressDialog.hide();
+                    }
+                    goToLogin();
+                    Log.d("Error.response", error.getMessage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    progressDialog.hide();
+                }
             }
         }){
             @Override
@@ -151,6 +118,7 @@ public class MainActivity extends Activity {
 
         AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(orderRequestList, REQUEST_TAG);
     }
+
     public void addButtons(List<String> stringArrayList, LinearLayout linearLayout) {
         for (final String i: stringArrayList) {
 
@@ -171,4 +139,14 @@ public class MainActivity extends Activity {
             linearLayout.addView(button);
         }
     }
+
+    public void toastMessage(String txt) {
+        Toast.makeText(getApplicationContext(), txt, Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
